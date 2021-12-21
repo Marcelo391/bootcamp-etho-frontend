2075f9ffea613ad6bf27a33c33d9bda112dda625
Marcelo Oliveira <marcelo_rde@yahoo.com.br>
2021-12-20 22:10:36 -0300

    Acrescentando o Material UI

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import Button from '../../components/buttons/default/button.component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { Container, Grid } from '@mui/material';
 
 export default function Login(){
 
     return (
         <React.Fragment>            
-            <Button primary >Entrar</Button>        
-            <Button secondary >
-                <FontAwesomeIcon className='iconSair' icon={faSignOutAlt} />
-                <span >Sair da Conta</span>  
-            </Button>
+            <Container>
+            <Grid container direction="column" justifyContent="center">
+                <Grid item xs={4}>
+                    <Button primary >Entrar</Button>        
+                </Grid>
+                <Grid item xs={4}>
+                    <Button secondary >
+                        <FontAwesomeIcon className='iconSair' icon={faSignOutAlt} />
+                        <span >Sair da Conta</span>  
+                    </Button>
+                </Grid>
+            </Grid>    
+            </Container>            
         </React.Fragment>
          
     )
